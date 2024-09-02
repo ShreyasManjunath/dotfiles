49ed9b84558f82d1197401e59260d3d14669f719
Shreyas Manjunath <shreyas.manjunath@node-robotics.com>
2024-09-02 23:44:56 +0200

    fix(keymaps): save buffer with ctrl+s

--- .config/nvim/lua/shreyas/core/keymaps.lua
+++ .config/nvim/lua/shreyas/core/keymaps.lua
@@ -4,17 +4,20 @@ local keymap = vim.keymap
 
 -- increment and decrement numbers
 
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Inc number"})
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number"})
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Inc number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- window management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab"})
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "close current tab"})
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab"})
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to prev tab"})
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to prev tab" })
 
-keymap.set("n", "<leader>s|", "<C-w>v", { desc = "Window vertical split"})
-keymap.set("n", "<leader>s-", "<C-w>s", { desc = "Window horizontal split"})
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make equal split"})
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split"})
+keymap.set("n", "<leader>s|", "<C-w>v", { desc = "Window vertical split" })
+keymap.set("n", "<leader>s-", "<C-w>s", { desc = "Window horizontal split" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make equal split" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
+-- ctrl + s for saving the buffer
+keymap.set("n", "<C-s>", ":w<CR>", { desc = "Ctrl + S => Save buffer" })
+keymap.set("i", "<C-s>", "<ESC>:w<CR>", { desc = "Ctrl + S => Save buffer" })
